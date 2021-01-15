1b3598b11f613cbcd11eb26040c4e320f340234c
Jeebaek <70047856+Jeebaek@users.noreply.github.com>
2021-01-16 02:02:47 +0900

    Update 사분면.js

--- 210108/사분면.js
+++ 210108/사분면.js
@@ -7,27 +7,40 @@
 // 제 4사분면은 x > 0 && y < 0
 
 
-function getQuadrant( x, y) {
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-    if ( x < -1000 || x > 1000 || x == 0) {
-        return;
-    }
+    
+let input = []
+rl.on("line", (line) => { 
+    input.push(line);
+    
+    let x = parseInt(input[0]);
+    let y = parseInt(input[1]);
 
-    if ( y < -1000 || y > 1000 || y == 0) {
-        return;
-    }
+    if ( x < -1000 || x > 1000 || x == 0) 
+        process.exit();
+  
+
+    if ( y < -1000 || y > 1000 || y == 0)
+        process.exit();
+  
 
     if (x > 0 && y > 0) {
-        console.log('1');
+        console.log("1");
     } else if (x < 0 && y > 0) {
-        console.log('2');
+        console.log("2");
     } else if (x < 0 && y < 0) {
-        console.log('3');
+        console.log("3");
     } else if (x > 0 && y < 0) {
-        console.log('4');
+        console.log("4");
     } 
+}).on("close", () => {
+    
 
-
-}
-
-    getQuadrant( 10, 35);
+    process.exit();
+    
+});
