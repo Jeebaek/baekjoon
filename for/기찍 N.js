46164bb32cd0cb219922b968f4a43e227aea52a7
Jeebaek <70047856+Jeebaek@users.noreply.github.com>
2021-01-19 23:10:22 +0900

    Update 기찍 N.js

--- for/기찍 N.js
+++ for/기찍 N.js
@@ -6,20 +6,17 @@
 //앞에서 한 N찍기 문제에서 반대로 결과가 나오도록 해야함->그렇기 위해서는 n값에서 1씩 감소하는 반복문을 만들어야한다
 //n의 조건은 (1000,000 >= 0)
 
-function getNumber(n) {
-    if (n < 1 || n > 100000) {
-      return;
-    }
-  
-    var arrNumber = new Array();
-  
-    for (var i = n; i > 0; i--) {
-      arrNumber[i] = i;
-    }
-  
-    for (var i = n; i > 0; i--) {
-      console.log(arrNumber[i]);
-    }
-  }
-  
-    getNumber();
+let input = Number(require('fs').readFileSync('/dev/stdin').toString());
+
+let answer = '';
+
+if (input < 1 || input > 100000)
+     process.exit();
+ 
+for (let i = input; i >= 1; i--) {
+    answer += i + "\n";
+}
+
+console.log(answer);
+
+// readline으로 풀려고 했으나 시간초과로 계속 실패해서 검색 후 fs 모듈 이용해서 풀었음 -> 이 문제 이용하여 N찍기 풀음
