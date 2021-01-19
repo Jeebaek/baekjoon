e545b4c327afde094f36be1e9aeacb75e38cbe44
Jeebaek <70047856+Jeebaek@users.noreply.github.com>
2021-01-19 12:32:35 +0900

    Update 구구단.js

--- for/구구단.js
+++ for/구구단.js
@@ -11,12 +11,27 @@
 //    console.log(i);
 
 
-function getNumber(n) {
-
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+ 
+let input = [];
+rl.on("line", (line) => { 
+    input.push(line);
+    
+   let n = parseInt(input[0]);
+  
+    if (n < 1 || n > 9) 
+         process.exit();
+   
     for (var i = 1; i <= 9; i++) {
-      console.log( n + "*" + i + "=" + n * i);
+      console.log( n + " * " + i + " = " + n * i);
     }
     
-    }
-    
-    getNumber(2)
+  
+}).on("close", () => {
+
+  process.exit();
+});
