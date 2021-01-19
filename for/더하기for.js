925c6b9b5439e2bd3f20a4b731ac34afc3ebd912
Jeebaek <70047856+Jeebaek@users.noreply.github.com>
2021-01-19 12:38:18 +0900

    Update 더하기for.js

--- for/더하기for.js
+++ for/더하기for.js
@@ -12,24 +12,31 @@
 
 
 
-    function getNumber(a,b) {
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+ 
+let input = []
+rl.on("line", (line) => { 
+  const array = line.split(" ");
+  const result = [] 
+  for (var i =0; i < array.length; i++){
+      result.push(parseInt(array[i]))
+      if(result[0] <=0 || result[1] >= 10){
+        process.exit();
+      }
+    }
   
-        if(a <= 0 || a >= 10) {
-          return;
-        }
-      
-        if(b <= 0 || b >= 10) {
-            return;
-        }
-      
-        let t = 3
-        for (var i = 1 ; i <= t ; i++) {
-          console.log(a+b);
-        }
-        }
-      
-        getNumber(4,100)
-        
-      
+  input.push(result)
 
-        // a와 b를 더한값이 t번 나온다
+}).on("close", () => {
+  let t = input[0];
+  for (let i = 1; i <= t; i++){
+    console.log(input[i][0] + input[i][1]);
+  }
+
+  process.exit();
+});
+          
