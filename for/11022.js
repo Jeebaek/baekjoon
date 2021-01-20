ee1f3171b67c0d2bc719ee385b935aaebe0b12d4
Jeebaek <70047856+Jeebaek@users.noreply.github.com>
2021-01-20 22:44:04 +0900

    Update 11022.js

--- for/11022.js
+++ for/11022.js
@@ -25,6 +25,8 @@ rl.on('line', line => {
     let A = parseInt(input[i][0]);
     let B = parseInt(input[i][1]);
     result += `Case #${i}: ${A} + ${B} = ${A+B}\n`;
+//   이 부분에서 계속 안돼서 찾아보니 숫자나 문자를 변수와 결합할 때에는 초기값을 지정해줘야 함 -> let result = ' ';
+//  이후 result += `Case #${i}: ${A} + ${B} = ${A+B}\n`; 백틱(`) 을 이용해서 어떻게 출력되어야 하는지 쓰고 result에 
   }
   console.log(result);
   process.exit();
